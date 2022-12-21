beb158aa25e2071f9364bdd8cffe258b11303172
Lei shen <shenl024@hotmail.com>
2022-12-20 21:59:32 -0500

    start project puzzlegame

---
+++ src/com/itheima/ui/LoginJFrame.java
@@ -0,0 +1,10 @@
+package com.itheima.ui;
+
+import javax.swing.*;
+
+public class LoginJFrame extends JFrame {
+    public  LoginJFrame(){
+        this.setSize(48, 600);
+        this.setVisible(true);
+    }
+}
---
+++ src/com/itheima/ui/Test.java
@@ -0,0 +1,23 @@
+package com.itheima.ui;
+
+import javax.swing.*;
+
+public class Test {
+    public static void main(String[] args){
+        JFrame gameJframe = new JFrame();
+
+        gameJframe.setSize(603, 680);
+        gameJframe.setVisible(true);
+
+
+        JFrame loginJframe = new JFrame();
+        loginJframe.setSize(488, 430);
+        gameJframe.setVisible(true);
+
+        JFrame registerJframe = new JFrame();
+        registerJframe.setSize(488, 500);
+        registerJframe.setVisible(true);
+
+
+    }
+}
