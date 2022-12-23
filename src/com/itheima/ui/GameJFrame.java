bfb1d46ebdf8e6d04ed5bccc5160f29597cfcc59
Lei shen <shenl024@hotmail.com>
2022-12-22 21:40:22 -0500

    update project puzzlegame

--- src/com/itheima/ui/GameJFrame.java
+++ src/com/itheima/ui/GameJFrame.java
@@ -1,9 +1,44 @@
 package com.itheima.ui;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class GameJFrame extends JFrame {
     public GameJFrame(){
+        initJFrame();
+
+
+        initJMenuBar();
+
+
+        this.setVisible(true);
+    }
+
+    private void initJMenuBar() {
+        //initialize user manu
+        JMenuBar jMenuBar = new JMenuBar();
+
+        JMenu functionJMenu = new JMenu("function");
+        JMenu aboutJMenu = new JMenu("about");
+
+        JMenuItem replayItem = new JMenuItem("restart game");
+        JMenuItem reLoginItem = new JMenuItem("Login again");
+        JMenuItem closeItem = new JMenuItem("close game");
+        JMenuItem accountItem = new JMenuItem("public account");
+
+        functionJMenu.add(replayItem);
+        functionJMenu.add(reLoginItem);
+        functionJMenu.add(closeItem);
+
+        aboutJMenu.add(accountItem);
+
+        jMenuBar.add(functionJMenu);
+        jMenuBar.add(aboutJMenu);
+
+        this.setJMenuBar(jMenuBar);
+    }
+
+    private void initJFrame() {
         //interface size
         this.setSize(603, 680);
         //title for the interface
@@ -14,11 +49,6 @@ public class GameJFrame extends JFrame {
         this.setLocationRelativeTo(null);
 
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-
-
-
-
-        this.setVisible(true);
     }
 
 }
